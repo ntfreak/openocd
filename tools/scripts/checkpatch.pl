b35c41027e4ab0dc7eb9c996529b90eabeb2723e
Paul Fertser <fercerpav@gmail.com>
2013-10-14 10:03:00 +0400

    checkpatch: add logging functions
    It's commonly considered that user-visible strings should not be split
    to different lines in the sources to ease grepping for them. Hence,
    checkpatch traditionally makes an exception for logging functions,
    lines having them can be of arbitrary length.
    
    OpenOCD uses different (from Linux, the kernel) names, so they need to
    be added to avoid false positives.
    
    This is the old commit bb3cd6ec438d ("checkpatch: add logging functions")
    re-applied.
    
    Change-Id: Ib18e4532cf7e1f79821b74c9bb6134a8a4e8be1b
    Signed-off-by: Paul Fertser <fercerpav@gmail.com>
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>

--- tools/scripts/checkpatch.pl
+++ tools/scripts/checkpatch.pl
@@ -482,8 +482,11 @@ our $logFunctions = qr{(?x:
 	WARN(?:_RATELIMIT|_ONCE|)|
 	panic|
 	MODULE_[A-Z_]+|
-	seq_vprintf|seq_printf|seq_puts
+	seq_vprintf|seq_printf|seq_puts|
+	# OpenOCD specific
+	LOG_(?:DEBUG|INFO|WARNING|ERROR|USER|USER_N|OUTPUT)+
 )};
+
 
 our $allocFunctions = qr{(?x:
 	(?:(?:devm_)?
