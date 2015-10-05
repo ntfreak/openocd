5a4b9b334dd7ec4832f133e9cf163bfe0d073573
Daniel Krebs <github@daniel-krebs.net>
2015-10-05 15:37:57 +0200

    rtos: add support for RIOT
    Add threads support for RIOT (https://github.com/RIOT-OS/RIOT).
    Original code is from Daniel Krebs.
    
    Change-Id: I83fe3b91dd75949e800b5aea1015d8fa37b09c61
    Signed-off-by: Daniel Krebs <github@daniel-krebs.net>
    Signed-off-by: Vincent Dupont <vincent@otakeys.com>
    Signed-off-by: Benjamin Valentin <benpicco@googlemail.com>

--- src/rtos/rtos.c
+++ src/rtos/rtos.c
@@ -38,6 +38,7 @@ extern struct rtos_type mqx_rtos;
 extern struct rtos_type uCOS_III_rtos;
 extern struct rtos_type nuttx_rtos;
 extern struct rtos_type hwthread_rtos;
+extern struct rtos_type riot_rtos;
 
 static struct rtos_type *rtos_types[] = {
 	&ThreadX_rtos,
@@ -50,7 +51,8 @@ static struct rtos_type *rtos_types[] = {
 	&mqx_rtos,
 	&uCOS_III_rtos,
 	&nuttx_rtos,
-	&hwthread_rtos,
+	&riot_rtos,
+	&hwthread_rtos, /* will always match */
 	NULL
 };
 
