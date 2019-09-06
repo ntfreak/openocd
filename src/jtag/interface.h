c5e5e6101fc8d700b2e7f0bfa34e31974d66ab45
Antonio Borneo <borneo.antonio@gmail.com>
2019-09-06 11:00:19 +0200

    hla: use the new system_reset API
    HLA uses its own internal driver's API to control the adapter's
    system reset, but at the same time it calls jtag_add_reset() to
    avoid breaking the internal logic of OpenOCD. This implicitly
    forces HLA to rely on jtag queue mechanism, even if HLA has no
    link with JTAG state machine. It requires HLA to implement an
    empty execute_queue() to comply with the JTAG queue.
    
    Modify the HLA framework and the HLA targets to use the new
    adapter API for system_reset and decouple HLA from JTAG queue.
    Rename the HLA static functions adapter_assert_reset() and
    adapter_deassert_reset() to avoid overlap with the global
    functions with same name.
    While there, fix a minor typo in a comment s/incase/in case/.
    
    Do not remove from HLA the JTAG specific API execute_queue(),
    even if not required anymore, because OpenOCD code still has
    calls to jtag_execute_queue() in case of non JTAG transport.
    
    Change-Id: I0e65e3e557bd665bd3d3aeaa84ea609b55a05e48
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>

--- src/jtag/interface.h
+++ src/jtag/interface.h
@@ -337,8 +337,8 @@ struct jtag_interface {
 extern const char * const jtag_only[];
 
 int adapter_resets(int assert_trst, int assert_srst);
-void adapter_assert_reset(void);
-void adapter_deassert_reset(void);
+int adapter_assert_reset(void);
+int adapter_deassert_reset(void);
 int adapter_config_trace(bool enabled, enum tpiu_pin_protocol pin_protocol,
 			 uint32_t port_size, unsigned int *trace_freq);
 int adapter_poll_trace(uint8_t *buf, size_t *size);
