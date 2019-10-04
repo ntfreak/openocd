98bb639eed745c902e508fb40788d192c0127cc4
Moritz Fischer <moritzf@google.com>
2019-10-03 20:25:26 -0700

    jtag: drivers: xlnx-pcie-xvc: Add support for Xilinx XVC/PCIe
    Add support for Xilinx Virtual Cable over PCIe JTAG controller.
    It is commonly used in Xilinx based PCI Express designs with JTAG IP
    in the FPGA fabric.
    
    Access to the JTAG registers happens via the PCI Express extended
    configuration space.
    
    This can be used to debug soft-cores instantiated in the FPGA fabric.
    
    The clang static checker doesn't find any new problems with this change.
    
    Change-Id: Ib12ede0d1f26dacfda808d5e05b947b640c5bde7
    Signed-off-by: Moritz Fischer <moritzf@google.com>

--- src/jtag/interfaces.c
+++ src/jtag/interfaces.c
@@ -117,6 +117,9 @@ extern struct jtag_interface opendous_interface;
 #if BUILD_SYSFSGPIO == 1
 extern struct jtag_interface sysfsgpio_interface;
 #endif
+#if BUILD_XLNX_PCIE_XVC == 1
+extern struct jtag_interface xlnx_pcie_xvc_interface;
+#endif
 #if BUILD_AICE == 1
 extern struct jtag_interface aice_interface;
 #endif
@@ -221,6 +224,9 @@ struct jtag_interface *jtag_interfaces[] = {
 #endif
 #if BUILD_SYSFSGPIO == 1
 		&sysfsgpio_interface,
+#endif
+#if BUILD_XLNX_PCIE_XVC == 1
+		&xlnx_pcie_xvc_interface,
 #endif
 #if BUILD_AICE == 1
 		&aice_interface,
