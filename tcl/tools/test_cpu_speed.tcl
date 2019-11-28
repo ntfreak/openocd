6dff435c57a97bceb90c4e9ddfe7df63f6e2d993
Marc Schink <dev@zapb.de>
2019-11-28 18:00:11 +0100

    tcl/tools: Add function to measure the speed of ARM Cortex-M devices
    Tested on an EFM32PG12 Starter Kit.
    
    Change-Id: I2cbc36fe0d2ad2089bf8c1e7d2260daaae4ddbb4
    Signed-off-by: Marc Schink <dev@zapb.de>

---
+++ tcl/tools/test_cpu_speed.tcl
@@ -0,0 +1,43 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Description:
+#  Measure the CPU clock frequency of an ARM Cortex-M based device.
+#
+# Return:
+#  The CPU clock frequency in Hz. A negative value indicates that the loop
+#  counter was saturated.
+#
+# Note:
+#  You may need to adapt the number of cycles for your device.
+#
+proc cortex_m_test_cpu_speed { address { timeout 200 } { cycles_per_loop 4 } } {
+	set loop_counter_start 0xffffffff
+
+	halt
+
+	# We place the following code at the given address to measure the
+	# CPU clock frequency:
+	#
+	# 3801: subs r0, #1
+	# d1fd: bne #-2
+	# e7fe: b #-4
+	array set loop_code {0 0xd1fd3801 1 0x0000e7fe}
+	array2mem loop_code 32 $address 2
+
+	reg pc $address
+	reg r0 $loop_counter_start
+	resume
+	sleep $timeout
+	halt
+
+	# Get the loop counter value from register r0.
+	reg2array cpu_speed_regs 0
+	set loop_counter_end $cpu_speed_regs(0)
+	set loop_counter_diff [expr $loop_counter_start - $loop_counter_end]
+
+	if { [expr {$loop_counter_end == 0}] } {
+		return -1
+	}
+
+	return [expr double($loop_counter_diff) * $cycles_per_loop / $timeout * 1000 ]
+}
