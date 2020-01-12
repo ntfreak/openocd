5238db5628e17022cc97b00c0cb26e7574eb575d
Tarek BOCHKATI <tarek.bouchkati@gmail.com>
2020-01-12 18:50:26 +0100

    flash/stm32l4: enhance protect handler to use efficiently all WRP areas
    stm32l4_protect: was using one WRP area per bank, without checking
    if it is already protecting some sectors.
    protection algo is more complicated than that, before using a WRP area
    we should check if it is already used, then either reuse it for extension
    (or reduction) or use a free area.
    
    Note: for some devices like STM32L4R/S in single bank mode, all 4 WRP areas
    are usable for that bank, to manage this case an attribute 'use_all_wrpxx'
    was introduced into stm32l4_part_info and used later in protection handlers
    
    To do that, helper/interval was introduced to optimize WRP areas' usage
    and a new wrp_desc was introduced to list the protected areas using WRP
    
    example usage:
    	$ telnet localhost 4444
    	> flash probe 0
    	  device idcode = 0x10036470 (STM32L4R/L4Sxx - Rev: Y)
    	  flash size = 2048kbytes
    	  flash mode : dual-bank
    	  flash 'stm32l4x' found at 0x08000000
    	> stm32l4x wrp_desc 0
    	  no protected areas
    	> flash protect 0 0 4 on
    	  set protection for sectors 0 through 4 on flash bank 0
    	> flash protect 0 8 9 on
    	  set protection for sectors 8 through 9 on flash bank 0
    	> stm32l4x wrp_desc 0
    	  protected areas: [0,4][8,9]
    	> flash protect 0 6 6 on
    	  the device WRPxy are not enough to set the requested protection
    	  failed setting protection for blocks 6 to 6
    	> flash protect 0 3 5 on
    	  set protection for sectors 3 through 5 on flash bank 0
    	> stm32l4x wrp_desc 0
    	  protected areas: [0,5][8,9]
    	> flash protect 0 6 7 on
    	  set protection for sectors 6 through 7 on flash bank 0
    	> stm32l4x wrp_desc 0
    	  protected areas: [0,9]
    	> flash protect 0 5 6 off
    	  cleared protection for sectors 5 through 6 on flash bank 0
    	> stm32l4x wrp_desc 0
    	  protected areas: [0,4][7,9]
    
    
    Change-Id: I6fc28346b4f6dd0217dd98bc585c5b4265cfe24d
    Signed-off-by: Tarek BOCHKATI <tarek.bouchkati@gmail.com>

---
+++ src/helper/interval.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *                                                                         *
+ *   Copyright (C) 2020 by Tarek Bochkati for STMicroelectronics           *
+ *   tarek.bouchkati@gmail.com                                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#ifndef OPENOCD_HELPER_INTERVAL_H
+#define OPENOCD_HELPER_INTERVAL_H
+
+typedef struct interval {
+	int start, end;
+	struct interval *next;
+} interval_t;
+
+int interval_count(interval_t *head);
+int interval_append(interval_t **head_ref, int start, int end);
+int interval_delete(interval_t **head_ref, int start, int end);
+int interval_reorder(interval_t **head_ref);
+void interval_destroy(interval_t *head);
+void interval_print_all(interval_t *head, char *str);
+
+
+#endif /* OPENOCD_HELPER_INTERVAL_H */
