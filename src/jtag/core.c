2f0eae2323bcf94d039fd497e88323e1b6af4261
Antonio Borneo <borneo.antonio@gmail.com>
2020-01-27 18:45:10 +0100

    swim: add new transport
    Add SWIM and STM8 to documentation and update TODO file.
    Introduce transport "swim" and command "swim newtap".
    Switch in swim.c from HLA API to the new SWIM API.
    Implement in stlink driver the SWIM APIs as wrappers of existing
    HLA functions.
    Remove any SWIM related reference from HLA files.
    Update stm8 config files and stlink-dap interface config file.
    
    Change-Id: I2bb9f58d52900f6eb4df05f979f7ef11fd439c24
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>

--- src/jtag/core.c
+++ src/jtag/core.c
@@ -2054,7 +2054,8 @@ int adapter_assert_reset(void)
 			jtag_add_reset(0, 1);
 		return ERROR_OK;
 	} else if (transport_is_swd() || transport_is_hla() ||
-			   transport_is_dapdirect_jtag() || transport_is_dapdirect_swd())
+			   transport_is_dapdirect_jtag() || transport_is_dapdirect_swd() ||
+			   transport_is_swim())
 		return adapter_system_reset(1);
 	else if (get_current_transport() != NULL)
 		LOG_ERROR("reset is not supported on %s",
@@ -2070,7 +2071,8 @@ int adapter_deassert_reset(void)
 		jtag_add_reset(0, 0);
 		return ERROR_OK;
 	} else if (transport_is_swd() || transport_is_hla() ||
-			 transport_is_dapdirect_jtag() || transport_is_dapdirect_swd())
+			   transport_is_dapdirect_jtag() || transport_is_dapdirect_swd() ||
+			   transport_is_swim())
 		return adapter_system_reset(0);
 	else if (get_current_transport() != NULL)
 		LOG_ERROR("reset is not supported on %s",
