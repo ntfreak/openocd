f2677a75878d4d9ea82395233dab81e8beac9129
Antonio Borneo <borneo.antonio@gmail.com>
2020-04-26 16:03:45 +0200

    [RFC] checkpath: check for spdx GPL-2.0, GPL-2.0-or-later, BSD-3-Clause
    The checkpatch from Linux kernel uses the external helper script
    spdxcheck.py to check for spdx tag in a set of possible license
    files.
    For OpenOCD the situation is far simpler, so keep it easy and
    embed in checkpatch the only used spdx tags:
    	GPL-2.0, GPL-2.0-or-later and BSD-3-Clause
    This removes the dependencies from python, the need to replicate
    the kernel's folders tree LICENSES and to adapt the scripts to the
    folder structure of OpenOCD.
    
    Actually we have a file GPL-3.0-or-later, contrib/itmdump.c, but
    it is only an independent helper not compiled with OpenOCD code.
    Other situations like this could or should be managed separately
    and checkpatch should help avoiding new code with incompatible
    license.
    
    Change-Id: I0a40da0127746217ee0ac416058d5ceb922428ff
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>

--- tools/scripts/checkpatch.pl
+++ tools/scripts/checkpatch.pl
@@ -898,6 +898,17 @@ sub is_maintained_obsolete {
 sub is_SPDX_License_valid {
 	my ($license) = @_;
 
+	$license =~ s/^SPDX-License-Identifier: //;
+	$license =~ s/[ \t\*\/]*$//;
+	return 1 if ("$license" eq "GPL-2.0-or-later");
+	return 1 if ("$license" eq "GPL-2.0");
+	return 1 if ("$license" eq "BSD-3-Clause");
+	return 0;
+}
+
+sub is_SPDX_License_valid_original_now_unused {
+	my ($license) = @_;
+
 	return 1 if (!$tree || which("python") eq "" || !(-e "$root/scripts/spdxcheck.py") || !(-e "$root/.git"));
 
 	my $root_path = abs_path($root);
