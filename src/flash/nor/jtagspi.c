ntfreak/openocd

commit 3c9bd7c6f30a34e3930e44dd2e8ce5f5a877b4eb

flash: nor: jtagspi: fix jtagspi_read_status() warning
Clang static analyzer says that in certain cases "Assigned value is
garbage or undefined" there.

Change-Id: Ib35a4cf7a553ba9461270a0dc4c4b9b205091e73
Signed-off-by: Paul Fertser <fercerpav@gmail.com>
Reviewed-on: http://openocd.zylin.com/4338
Tested-by: jenkins

src/flash/nor/jtagspi.c
@@ -224,9 +224,10 @@ static int jtagspi_probe(struct flash_bank *bank)
 static void jtagspi_read_status(struct flash_bank *bank, uint32_t *status)
 {
 	uint8_t buf;
-	jtagspi_cmd(bank, SPIFLASH_READ_STATUS, NULL, &buf, -8);
-	*status = buf;
-	/* LOG_DEBUG("status=0x%08" PRIx32, *status); */
+	if (jtagspi_cmd(bank, SPIFLASH_READ_STATUS, NULL, &buf, -8) == ERROR_OK) {
+		*status = buf;
+		/* LOG_DEBUG("status=0x%08" PRIx32, *status); */
+	}
 }
 
 static int jtagspi_wait(struct flash_bank *bank, int timeout_ms)

commit 12af1864a7bd1a9559518168fa45df404866be99

flash/nor/jtagspi: workaround for build error on gcc 10.1.0
The new inter-procedural analysis of gcc 10 identifies two cases
of variable potentially used uninitialized.
This can really happen if jtagspi_cmd() in jtagspi_read_status()
returns error and then 'status' don't get initialized either in
jtagspi_wait() or in jtagspi_write_enable().

This change is just a workaround to build openocd on new compiler.
The correct change should propagate the error, but I do not own
the HW to verify or test such changes.

Change-Id: I41fa25fc42dc01b5bc9aa55a108a58e89e157a35
Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
Fixes: 3c9bd7c6f30a ("flash: nor: jtagspi: fix jtagspi_read_status() warning")
Fixes: d25355473da9 ("flash/nor/jtagspi: add JTAGSPI driver")

src/flash/nor/jtagspi.c
@@ -239,7 +239,7 @@ static void jtagspi_read_status(struct flash_bank *bank, uint32_t *status)
 
 static int jtagspi_wait(struct flash_bank *bank, int timeout_ms)
 {
-	uint32_t status;
+	uint32_t status = 0;
 	int64_t t0 = timeval_ms();
 	int64_t dt;
 
@@ -259,7 +259,7 @@ static int jtagspi_wait(struct flash_bank *bank, int timeout_ms)
 
 static int jtagspi_write_enable(struct flash_bank *bank)
 {
-	uint32_t status;
+	uint32_t status = 0;
 
 	jtagspi_cmd(bank, SPIFLASH_WRITE_ENABLE, NULL, NULL, 0);
 	jtagspi_read_status(bank, &status);