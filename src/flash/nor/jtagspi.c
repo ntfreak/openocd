ntfreak/openocd

commit 3c9bd7c6f30a34e3930e44dd2e8ce5f5a877b4eb

flash: nor: jtagspi: fix jtagspi_read_status() warning
Clang static analyzer says that in certain cases "Assigned value is
garbage or undefined" there.

Change-Id: Ib35a4cf7a553ba9461270a0dc4c4b9b205091e73
Signed-off-by: Paul Fertser <fercerpav@gmail.com>
Reviewed-on: http://openocd.zylin.com/4338
Tested-by: jenkins

src/flash/nor/jtagspi.c
@@ -224,9 +224,10 @@ static int jtagspi_probe(struct flash_bank *bank)
 static void jtagspi_read_status(struct flash_bank *bank, uint32_t *status)
 {
 	uint8_t buf;
-	jtagspi_cmd(bank, SPIFLASH_READ_STATUS, NULL, &buf, -8);
-	*status = buf;
-	/* LOG_DEBUG("status=0x%08" PRIx32, *status); */
+	if (jtagspi_cmd(bank, SPIFLASH_READ_STATUS, NULL, &buf, -8) == ERROR_OK) {
+		*status = buf;
+		/* LOG_DEBUG("status=0x%08" PRIx32, *status); */
+	}
 }
 
 static int jtagspi_wait(struct flash_bank *bank, int timeout_ms)

commit 9abe4789300f201ea3b839f59df02cddf73b9527

flash/nor/jtagspi: fix build and propagate error reading status
The new inter-procedural analysis of gcc 10 identifies two cases
of variable potentially used uninitialized.
This can really happen if jtagspi_cmd() in jtagspi_read_status()
returns error and then 'status' don't get initialized either in
jtagspi_wait() or in jtagspi_write_enable().

Fix the code by propagating the error after read the status and
check the error in the callers to guarantee status has been read.
This also fixes the build error.

Change-Id: I41fa25fc42dc01b5bc9aa55a108a58e89e157a35
Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
Fixes: 3c9bd7c6f30a ("flash: nor: jtagspi: fix jtagspi_read_status() warning")
Fixes: d25355473da9 ("flash/nor/jtagspi: add JTAGSPI driver")

src/flash/nor/jtagspi.c
@@ -228,13 +228,17 @@ static int jtagspi_probe(struct flash_bank *bank)
 	return ERROR_OK;
 }
 
-static void jtagspi_read_status(struct flash_bank *bank, uint32_t *status)
+static int jtagspi_read_status(struct flash_bank *bank, uint32_t *status)
 {
 	uint8_t buf;
-	if (jtagspi_cmd(bank, SPIFLASH_READ_STATUS, NULL, &buf, -8) == ERROR_OK) {
-		*status = buf;
-		/* LOG_DEBUG("status=0x%08" PRIx32, *status); */
-	}
+	int retval;
+
+	retval = jtagspi_cmd(bank, SPIFLASH_READ_STATUS, NULL, &buf, -8);
+	if (retval != ERROR_OK)
+		return retval;
+
+	*status = buf;
+	return ERROR_OK;
 }
 
 static int jtagspi_wait(struct flash_bank *bank, int timeout_ms)
@@ -242,10 +246,13 @@ static int jtagspi_wait(struct flash_bank *bank, int timeout_ms)
 	uint32_t status;
 	int64_t t0 = timeval_ms();
 	int64_t dt;
+	int retval;
 
 	do {
 		dt = timeval_ms() - t0;
-		jtagspi_read_status(bank, &status);
+		retval = jtagspi_read_status(bank, &status);
+		if (retval != ERROR_OK)
+			return retval;
 		if ((status & SPIFLASH_BSY_BIT) == 0) {
 			LOG_DEBUG("waited %" PRId64 " ms", dt);
 			return ERROR_OK;
@@ -260,9 +267,12 @@ static int jtagspi_wait(struct flash_bank *bank, int timeout_ms)
 static int jtagspi_write_enable(struct flash_bank *bank)
 {
 	uint32_t status;
+	int retval;
 
 	jtagspi_cmd(bank, SPIFLASH_WRITE_ENABLE, NULL, NULL, 0);
-	jtagspi_read_status(bank, &status);
+	retval = jtagspi_read_status(bank, &status);
+	if (retval != ERROR_OK)
+		return retval;
 	if ((status & SPIFLASH_WE_BIT) == 0) {
 		LOG_ERROR("Cannot enable write to flash. Status=0x%08" PRIx32, status);
 		return ERROR_FAIL;