04a5adac582e05c54c33e7f0e7e1c4a77f4a46fe
Antonio Borneo <borneo.antonio@gmail.com>
2020-05-25 11:28:22 +0200

    target/arm9tdmi: fix memory leak of register cache
    There is no method to free the register cache, allocated in
    arm9tdmi_build_reg_cache(), so we get a memory leak.
    Issue identified by tracking all calls to arm_build_reg_cache().
    
    Implement the method arm9tdmi_deinit_target() that in turn calls
    arm9tdmi_free_reg_cache().
    
    NOT TESTED on real HW, I will rely on gerrit reviewer for tests.
    
    Change-Id: If2bed02c516051ce4d0eb29b204a3f3337fe5d6a
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>

--- src/target/arm9tdmi.c
+++ src/target/arm9tdmi.c
@@ -711,12 +711,24 @@ static void arm9tdmi_build_reg_cache(struct target *target)
 	(*cache_p) = arm_build_reg_cache(target, arm);
 }
 
+static void arm9tdmi_free_reg_cache(struct target *target)
+{
+	struct arm *arm = target_to_arm(target);
+
+	arm_free_reg_cache(arm);
+}
+
 int arm9tdmi_init_target(struct command_context *cmd_ctx,
 		struct target *target)
 {
 	arm9tdmi_build_reg_cache(target);
 	arm_semihosting_init(target);
 	return ERROR_OK;
+}
+
+void arm9tdmi_deinit_target(struct target *target)
+{
+	arm9tdmi_free_reg_cache(target);
 }
 
 int arm9tdmi_init_arch_info(struct target *target,
@@ -921,6 +933,7 @@ struct target_type arm9tdmi_target = {
 	.commands = arm9tdmi_command_handlers,
 	.target_create = arm9tdmi_target_create,
 	.init_target = arm9tdmi_init_target,
+	.deinit_target = arm9tdmi_deinit_target,
 	.examine = arm7_9_examine,
 	.check_reset = arm7_9_check_reset,
 };
