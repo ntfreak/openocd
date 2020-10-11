6143d7ac592984346c5572309f1b85ffa855a550
Daniel Anselmi <danselmi@gmx.ch>
2020-10-11 15:13:05 +0200

    Add IPDBG JtagHost functionality to OpenOCD
    IPDBG are utilities to debug IP-cores. It uses JTAG for
    transport to/from the FPGA. The different UIs use TCP/IP
    as transport. The JtagHost makes the bridge between these
    two.
    
    Comparable to the bridge between GDB and the in-circuit-
    debugging-unit of a micro controller.
    
    Change-Id: Ib1bc10dcbd4ea426e492bb7b2d85c1ed1b7a8d5a
    Signed-off-by: Daniel Anselmi <danselmi@gmx.ch>

---
+++ src/ipdbg/ipdbg.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* Copyright (C) 2020 by Daniel Anselmi <danselmi@gmx.ch> */
+
+#ifndef OPENOCD_IPDBG_IPDBG_H
+#define OPENOCD_IPDBG_IPDBG_H
+
+#include <helper/command.h>
+
+#define IPDBG_BUFFER_SIZE 16384
+
+int ipdbg_register_commands(struct command_context *cmd_ctx);
+
+#endif /* OPENOCD_IPDBG_IPDBG_H */
