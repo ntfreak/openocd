259b45a4e0b929f513c43f95c31cba37720c3ad9
Antonio Borneo <borneo.antonio@gmail.com>
2020-11-13 23:36:24 +0100

    armv7m_trace: get rid of the old tpiu code
    Remove all the old tpiu code and replace it with a wrapper that
    uses the new commands, prints-out the new commands and informs
    about the deprecation.
    
    All the code that handles the deprecated tpiu commands is enclosed
    between the comments
    	/* START_DEPRECATED_TPIU */
    and
    	/* END_DEPRECATED_TPIU */
    so will be easy to remove it in the future.
    
    Change-Id: I70b0486770128203b923346382d9a90b8ec08439
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>

--- src/target/target.c
+++ src/target/target.c
@@ -4934,6 +4934,11 @@ no_params:
 				}
 
 				if (goi->isconfigure) {
+					/* START_DEPRECATED_TPIU */
+					if (n->value == TARGET_EVENT_TRACE_CONFIG)
+						LOG_INFO("DEPRECATED target event %s", n->name);
+					/* END_DEPRECATED_TPIU */
+
 					bool replace = true;
 					if (teap == NULL) {
 						/* create new */
