9a826e5d11393d71050dca196b0b5224979a8ee5
Antonio Borneo <borneo.antonio@gmail.com>
2020-12-05 23:45:57 +0100

    flash/nor/stmqspi: fix compile error with clang 12.0.0
    The unofficial version of clang 12.0.0 available for Mac Mini M1
    	Apple clang version 12.0.0 (clang-1200.0.32.27)
    	Target: arm64-apple-darwin20.1.0
    detects a set of incorrect format types.
    When variables of type uint8_t or uint16_t are logic-and with an
    'unsigned int', the result is 'unsigned int' and format type has
    to be '%x', not 'PRIx8' nor 'PRIx16'.
    
    Change-Id: I64b9c24938b0090fbc117b8d5312f462e52af516
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reported-by: Xiaofan Chen <xiaofanc@gmail.com>

--- src/flash/nor/stmqspi.c
+++ src/flash/nor/stmqspi.c
@@ -455,7 +455,7 @@ static int qspi_write_enable(struct flash_bank *bank)
 	if ((stmqspi_info->saved_cr & (BIT(SPI_DUAL_FLASH) | BIT(SPI_FSEL_FLASH)))
 		!= BIT(SPI_FSEL_FLASH))
 		if ((status & (SPIFLASH_WE_BIT | SPIFLASH_BSY_BIT)) != SPIFLASH_WE_BIT) {
-			LOG_ERROR("Cannot write enable flash1. Status=0x%02" PRIx8,
+			LOG_ERROR("Cannot write enable flash1. Status=0x%02x",
 				status & 0xFF);
 			return ERROR_FLASH_OPERATION_FAILED;
 		}
@@ -464,7 +464,7 @@ static int qspi_write_enable(struct flash_bank *bank)
 	status >>= 8;
 	if ((stmqspi_info->saved_cr & (BIT(SPI_DUAL_FLASH) | BIT(SPI_FSEL_FLASH))) != 0)
 		if ((status & (SPIFLASH_WE_BIT | SPIFLASH_BSY_BIT)) != SPIFLASH_WE_BIT) {
-			LOG_ERROR("Cannot write enable flash2. Status=0x%02" PRIx8,
+			LOG_ERROR("Cannot write enable flash2. Status=0x%02x",
 				status & 0xFF);
 			return ERROR_FLASH_OPERATION_FAILED;
 		}
@@ -548,7 +548,7 @@ COMMAND_HANDLER(stmqspi_handle_mass_erase_command)
 	if (((stmqspi_info->saved_cr & (BIT(SPI_DUAL_FLASH) | BIT(SPI_FSEL_FLASH)))
 		!= BIT(SPI_FSEL_FLASH)) && ((status & SPIFLASH_BSY_BIT) == 0) &&
 		((status & SPIFLASH_WE_BIT) != 0)) {
-		LOG_ERROR("Mass erase command not accepted by flash1. Status=0x%02" PRIx8,
+		LOG_ERROR("Mass erase command not accepted by flash1. Status=0x%02x",
 			status & 0xFF);
 		retval = ERROR_FLASH_OPERATION_FAILED;
 		goto err;
@@ -559,7 +559,7 @@ COMMAND_HANDLER(stmqspi_handle_mass_erase_command)
 	if (((stmqspi_info->saved_cr & (BIT(SPI_DUAL_FLASH) | BIT(SPI_FSEL_FLASH))) != 0) &&
 		((status & SPIFLASH_BSY_BIT) == 0) &&
 		((status & SPIFLASH_WE_BIT) != 0)) {
-		LOG_ERROR("Mass erase command not accepted by flash2. Status=0x%02" PRIx8,
+		LOG_ERROR("Mass erase command not accepted by flash2. Status=0x%02x",
 			status & 0xFF);
 		retval = ERROR_FLASH_OPERATION_FAILED;
 		goto err;
@@ -959,7 +959,7 @@ static int qspi_erase_sector(struct flash_bank *bank, unsigned int sector)
 	if (((stmqspi_info->saved_cr & (BIT(SPI_DUAL_FLASH) | BIT(SPI_FSEL_FLASH)))
 		!= BIT(SPI_FSEL_FLASH)) && ((status & SPIFLASH_BSY_BIT) == 0) &&
 		((status & SPIFLASH_WE_BIT) != 0)) {
-		LOG_ERROR("Sector erase command not accepted by flash1. Status=0x%02" PRIx8,
+		LOG_ERROR("Sector erase command not accepted by flash1. Status=0x%02x",
 			status & 0xFF);
 		retval = ERROR_FLASH_OPERATION_FAILED;
 		goto err;
@@ -971,7 +971,7 @@ static int qspi_erase_sector(struct flash_bank *bank, unsigned int sector)
 	if (((stmqspi_info->saved_cr & (BIT(SPI_DUAL_FLASH) | BIT(SPI_FSEL_FLASH))) != 0) &&
 		((status & SPIFLASH_BSY_BIT) == 0) &&
 		((status & SPIFLASH_WE_BIT) != 0)) {
-		LOG_ERROR("Sector erase command not accepted by flash2. Status=0x%02" PRIx8,
+		LOG_ERROR("Sector erase command not accepted by flash2. Status=0x%02x",
 			status & 0xFF);
 		retval = ERROR_FLASH_OPERATION_FAILED;
 		goto err;
@@ -1214,7 +1214,7 @@ static int stmqspi_blank_check(struct flash_bank *bank)
 			/* we need le_32_to_h, but that's the same as h_to_le_32 */
 			result = h_to_le_32(erase_check_info.result);
 			bank->sectors[sector + index].is_erased = ((result & 0xFF) == 0xFF);
-			LOG_DEBUG("Flash sector %u checked: 0x%04" PRIx16, sector + index, result & 0xFFFF);
+			LOG_DEBUG("Flash sector %u checked: 0x%04x", sector + index, result & 0xFFFF);
 		}
 		keep_alive();
 		sector += count;
