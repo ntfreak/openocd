10c36e8934692e6793e4f539943e1be9c0645e0f
Tarek BOCHKATI <tarek.bouchkati@st.com>
2021-02-12 21:12:43 +0100

    cortex_m: avoid reading and writing non-existent registers
    Change-Id: Iedc24352c8d3444372da06d00fcec9603540f950
    Signed-off-by: Tarek BOCHKATI <tarek.bouchkati@st.com>

--- src/target/armv7m.c
+++ src/target/armv7m.c
@@ -166,10 +166,10 @@ int armv7m_restore_context(struct target *target)
 	 * packing of ARMV7M_PMSK_BPRI_FLTMSK_CTRL!
 	 * See also comments in the register table above */
 	for (i = cache->num_regs - 1; i >= 0; i--) {
-		if (cache->reg_list[i].dirty) {
-			armv7m->arm.write_core_reg(target, &cache->reg_list[i], i,
-						   ARM_MODE_ANY, cache->reg_list[i].value);
-		}
+		struct reg *reg = &cache->reg_list[i];
+
+		if (reg->exist && reg->dirty)
+			armv7m->arm.write_core_reg(target, reg, i, ARM_MODE_ANY, reg->value);
 	}
 
 	return ERROR_OK;
