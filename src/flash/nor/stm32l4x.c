cd4dfcb28c4949d312e7d4f1bb5a6081aca5fdb2
Tarek BOCHKATI <tarek.bouchkati@gmail.com>
2021-02-14 21:46:09 +0100

    flash/stm32l4x: add optional flash bank arguments for DBGMCU_IDCODE
    this is mainly to workaround an issue with some STM32WL5x early cuts,
    which cannot read the DBGMCU registers from AP1 (using the second core)
    
    Change-Id: Idb4ce8eeb765d964548248304049a33fc5bccc1e
    Signed-off-by: Tarek BOCHKATI <tarek.bouchkati@gmail.com>

--- src/flash/nor/stm32l4x.c
+++ src/flash/nor/stm32l4x.c
@@ -537,7 +537,7 @@ static const struct stm32l4_part_info stm32l4_parts[] = {
 	},
 };
 
-/* flash bank stm32l4x <base> <size> 0 0 <target#> */
+/* flash bank stm32l4x <base> <size> 0 0 <target#> [mcu_idcode] */
 FLASH_BANK_COMMAND_HANDLER(stm32l4_flash_bank_command)
 {
 	struct stm32l4_flash_bank *stm32l4_info;
@@ -561,6 +561,10 @@ FLASH_BANK_COMMAND_HANDLER(stm32l4_flash_bank_command)
 	stm32l4_info->probed = false;
 	stm32l4_info->otp_enabled = false;
 	stm32l4_info->user_bank_size = bank->size;
+
+	stm32l4_info->idcode = 0;
+	if (CMD_ARGC == 7)
+		COMMAND_PARSE_NUMBER(u32, CMD_ARGV[6], stm32l4_info->idcode);
 
 	return ERROR_OK;
 }
@@ -1533,13 +1537,16 @@ static int stm32l4_probe(struct flash_bank *bank)
 	const struct stm32l4_part_info *part_info;
 	uint16_t flash_size_kb = 0xffff;
 	uint32_t device_id;
+	int retval;
 
 	stm32l4_info->probed = false;
 
-	/* read stm32 device id registers */
-	int retval = stm32l4_read_idcode(bank, &stm32l4_info->idcode);
-	if (retval != ERROR_OK)
-		return retval;
+	/* read stm32 device id registers, if not specified by the user */
+	if (stm32l4_info->idcode == 0) {
+		retval = stm32l4_read_idcode(bank, &stm32l4_info->idcode);
+		if (retval != ERROR_OK)
+			return retval;
+	}
 
 	device_id = stm32l4_info->idcode & 0xFFF;
 
