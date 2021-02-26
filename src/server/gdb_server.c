ntfreak/openocd

commit d301d8b42f0bfe67d76d6f340db6570cc71c876e

gdb_server: add support for vCont
Make gdb use target support for single-stepping if available.

Change-Id: Ie72345a1e749aefba7cd175ccbf5cf51d4f1a632
Signed-off-by: Matthias Welwarsky <matthias.welwarsky@sysgo.com>
Reviewed-on: http://openocd.zylin.com/3833
Tested-by: jenkins
Reviewed-by: Tomas Vanek <vanekt@fbl.cz>

src/server/gdb_server.c
@@ -41,6 +41,8 @@
 #include <target/breakpoints.h>
 #include <target/target_request.h>
 #include <target/register.h>
+#include <target/target.h>
+#include <target/target_type.h>
 #include "server.h"
 #include <flash/nor/core.h>
 #include "gdb_server.h"
@@ -109,6 +111,8 @@ static char *gdb_port_next;
 
 static void gdb_log_callback(void *priv, const char *file, unsigned line,
 		const char *function, const char *string);
+
+static void gdb_sig_halted(struct connection *connection);
 
 /* number of gdb connections, mainly to suppress gdb related debugging spam
  * in helper/log.c when no gdb connections are actually active */
@@ -2470,7 +2474,7 @@ static int gdb_query_packet(struct connection *connection,
 			&buffer,
 			&pos,
 			&size,
-			"PacketSize=%x;qXfer:memory-map:read%c;qXfer:features:read%c;qXfer:threads:read+;QStartNoAckMode+",
+			"PacketSize=%x;qXfer:memory-map:read%c;qXfer:features:read%c;qXfer:threads:read+;QStartNoAckMode+;vContSupported+",
 			(GDB_BUFFER_SIZE - 1),
 			((gdb_use_memory_map == 1) && (flash_get_bank_count() > 0)) ? '+' : '-',
 			(gdb_target_desc_supported == 1) ? '+' : '-');
@@ -2559,6 +2563,90 @@ static int gdb_query_packet(struct connection *connection,
 	return ERROR_OK;
 }
 
+static bool gdb_handle_vcont_packet(struct connection *connection, const char *packet, int packet_size)
+{
+	struct gdb_connection *gdb_connection = connection->priv;
+	struct target *target = get_target_from_connection(connection);
+	const char *parse = packet;
+	int retval;
+
+	/* query for vCont supported */
+	if (parse[0] == '?') {
+		if (target->type->step != NULL) {
+			/* gdb doesn't accept c without C and s without S */
+			gdb_put_packet(connection, "vCont;c;C;s;S", 13);
+			return true;
+		}
+		return false;
+	}
+
+	if (parse[0] == ';') {
+		++parse;
+		--packet_size;
+	}
+
+	/* simple case, a continue packet */
+	if (parse[0] == 'c') {
+		LOG_DEBUG("target %s continue", target_name(target));
+		log_add_callback(gdb_log_callback, connection);
+		retval = target_resume(target, 1, 0, 0, 0);
+		if (retval == ERROR_OK) {
+			gdb_connection->frontend_state = TARGET_RUNNING;
+			target_call_event_callbacks(target, TARGET_EVENT_GDB_START);
+		}
+		return true;
+	}
+
+	/* single-step or step-over-breakpoint */
+	if (parse[0] == 's') {
+		if (strncmp(parse, "s:", 2) == 0) {
+			int handle_breakpoint = 1;
+			struct target *ct = target;
+			int64_t thread_id;
+			char *endp;
+
+			parse += 2;
+			packet_size -= 2;
+
+			thread_id = strtoll(parse, &endp, 16);
+			if (endp != NULL) {
+				packet_size -= endp - parse;
+				parse = endp;
+			}
+
+			if (parse[0] == ';') {
+				++parse;
+				--packet_size;
+
+				if (parse[0] == 'c') {
+					parse += 1;
+					packet_size -= 1;
+
+					handle_breakpoint = 0;
+				}
+			}
+
+			LOG_DEBUG("target %s single-step thread %"PRId64, target_name(ct), thread_id);
+			retval = target_step(ct, 1, 0, handle_breakpoint);
+			if (retval == ERROR_OK) {
+				gdb_signal_reply(target, connection);
+				/* stop forwarding log packets! */
+				log_remove_callback(gdb_log_callback, connection);
+			} else
+			if (retval == ERROR_TARGET_TIMEOUT) {
+				gdb_connection->frontend_state = TARGET_RUNNING;
+				target_call_event_callbacks(ct, TARGET_EVENT_GDB_START);
+			}
+		} else {
+			LOG_ERROR("Unknown vCont packet");
+			return false;
+		}
+		return true;
+	}
+
+	return false;
+}
+
 static int gdb_v_packet(struct connection *connection,
 		char const *packet, int packet_size)
 {
@@ -2567,6 +2655,19 @@ static int gdb_v_packet(struct connection *connection,
 	int result;
 
 	target = get_target_from_connection(connection);
+
+	if (strncmp(packet, "vCont", 5) == 0) {
+		bool handled;
+
+		packet += 5;
+		packet_size -= 5;
+
+		handled = gdb_handle_vcont_packet(connection, packet, packet_size);
+		if (!handled)
+			gdb_put_packet(connection, "", 0);
+
+		return ERROR_OK;
+	}
 
 	/* if flash programming disabled - send a empty reply */
 

commit c53ac5214e9381cce8c831ab19003a77837216c9

gdb_server: fix detection of target halt after gdb 'continue'
OpenOCD is based on a single main loop that schedules all the
activities.
At the execution of a TCL command, the timestamp is checked to
eventually trigger the polling of the targets. This means that by
executing a TCL command the polling can be triggered and detect a
change of target's state.
This can be a problem if a TCL command has to be executed during a
code sequence that is already changing the target's state (e.g.
halt, resume or single-step) and the undesired poll can cause a
lost of sync between the code sequence and openocd target's state.

The gdb command 'continue' causes gdb_handle_vcont_packet() to run
target_resume(), that in turn runs the target event 'resumed'
after the successful restart of the target.
If an handler is associated to the event above, the execution of
the TCL commands in the handler can trigger the polling.
The target is just restarted, but could have already hit another
breakpoint so the new state, that should be 'running', could be
again 'halted'!
This early return in 'halted' state during the resume process
prevents gdb_server to inform gdb about the new halt, and gdb
keeps waiting for target to halt.

This can be verified by running on the target a firmware with a
loop, run openocd with a dummy 'resumed' event, and let gdb to set
a breakpoint in the loop. A 'continue' command will cause the
target to halt again by hitting the breakpoint at the next loop
iteration, but gdb will loose it and will not return the prompt.

	openocd -f board/st_nucleo_f4.cfg -c \
		'stm32f4x.cpu configure -event resumed {echo hello}'
	arm-none-eabi-gdb -ex 'target remote :3333' -ex 'b *$pc' -ex c

Disable the polling while executing target_resume().

Change-Id: I3be830a8e7c2ef6278617cb4547a4d676b0ddeb5
Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
Reported-by: Габитов Александр Фаритович <gabitov@planarchel.ru>
Fixes: d301d8b42f0b ("gdb_server: add support for vCont")

src/server/gdb_server.c
@@ -2743,7 +2743,17 @@ static bool gdb_handle_vcont_packet(struct connection *connection, const char *p
 		gdb_running_type = 'c';
 		LOG_DEBUG("target %s continue", target_name(target));
 		log_add_callback(gdb_log_callback, connection);
+		/*
+		 * target_resume() triggers the event 'resumed'. The execution of TCL
+		 * commands in the event handler causes the polling of targets.
+		 * We need to complete all the internal openocd steps before allowing
+		 * polling to change target's state.
+		 * Disable polling during target_resume().
+		 */
+		bool save_poll = jtag_poll_get_enabled();
+		jtag_poll_set_enabled(false);
 		retval = target_resume(target, 1, 0, 0, 0);
+		jtag_poll_set_enabled(save_poll);
 		if (retval == ERROR_TARGET_NOT_HALTED)
 			LOG_INFO("target %s was not halted when resume was requested", target_name(target));
 